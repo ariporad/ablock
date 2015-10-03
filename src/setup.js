4ff136381d273f364e236745eb4a5e575388c649
Ari Porad <git@ariporad.com>
2015-10-03 09:14:30 -0700

    refactor(setup): don't use babel private variables

--- src/setup.js
+++ src/setup.js
@@ -2,14 +2,17 @@
 /* istanbul ignore next: this doesn't need to be tested */
 if (!global._hasDoneSetup) {
   global._hasDoneSetup = true;
-  const Bluebird = require('bluebird');
 
-// Just for performance
-  if (global.Promise !== Bluebird) global.Promise = Bluebird;
+  if(!global.Bluebird) global.Bluebird = require('bluebird');
+  global.Promise = global.Bluebird;
 
 // This will throw an error if loaded twice.
 // This will also use Bluebird promises.
-  if (!global._babelPolyfill) require('babel/polyfill');
+  try {
+    require('babel/polyfill');
+  } catch (e) {
+    // Babel throws an error if loaded more than once. We don't care.
+  }
 
 // This has a build in multiple call check
   require('app-module-path').addPath(__dirname);
