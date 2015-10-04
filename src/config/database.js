b6a2708d301798b05e9ef7cb982871978e02de22
Ari Porad <git@ariporad.com>
2015-10-03 19:45:19 -0700

    feat(database): don't log every SQL query

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 /* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  // Don't log every SQL query
+  logging: false, // TODO: make configurable (verbose mode?)
+});
 
 module.exports = sequelize;
