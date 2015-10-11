b194a04158e55fe757c3e78efc697a5d77877970
Ari Porad <git@ariporad.com>
2015-10-11 08:51:59 -0700

    chore(gulp): don't use shelljs anymore

--- gulp/travis.js
+++ gulp/travis.js
@@ -1,6 +1,6 @@
 /* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 const gulp = require('gulp');
-const shell = require('shelljs');
+const exec = require('child_process');
 const { resolve } = require('path');
 const { basePath } = require('./lib/config');
 const testCov = require('./test-cov');
@@ -8,17 +8,20 @@ const testCov = require('./test-cov');
 
 const lcov = resolve(basePath, 'coverage', 'lcov.info');
 const coveralls = resolve(basePath, 'node_modules', '.bin', 'coveralls');
-// Each item is one command.
-const coverageUploadCommand = [
-  `cd ${basePath}`,
-  `cat ${lcov} | ${coveralls}`,
-].join(';');
+const coverageUploadCommand = `cat ${lcov} | ${coveralls}`;
 
 const uploadCoverage = () => {
   // Only upload coverage once
   if ((process.env.TRAVIS_JOB_NUMBER || '0.1').split('.').pop() === '1') return;
 
-  return shell.exec(coverageUploadCommand);
+  return new Promise((good, bad) => {
+    exec(coverageUploadCommand, (err, stdout, stderr) => {
+      console.log(stdout);
+      console.error(stderr);
+      console.error(err);
+      good();
+    });
+  });
 };
 
 const travis = () => {
